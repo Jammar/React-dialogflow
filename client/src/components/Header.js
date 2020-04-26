fb47d9ab846a6c5be81729117ac2cf5a76b802ab
Jammar <46968969+Jammar@users.noreply.github.com>
2020-04-26 11:46:04 +0200

    Fixed Header define styled-component outside of component, also cleaned up the return function a bit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,27 +4,24 @@ import styled from 'styled-components'
 
 import logo from '../assets/images/gemensam_logogtyp.png'
 
-const Header = () => {
-  const Head = styled.header`
-    height: 10vh;
-    width: 100%;
-    max-width: 1000px;
-    margin: 0 auto;
-    margin-bottom: 1vh;
+const Head = styled.header`
+  height: 10vh;
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  margin-bottom: 1vh;
 
-    img {
-      width: auto;
-      height: auto;
-      max-width: 100%;
-      max-height: 100%;
-    }
-  `
-
-  return (
+  img {
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 100%;
+  }
+`
+const Header = () => (
     <Head>
       <img src={logo} alt="Länsförsäkringars gemensamma logotyp" />
     </Head>
-  )
-}
+)
 
 export default Header
